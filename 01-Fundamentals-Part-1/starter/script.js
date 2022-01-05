171d6aeea72e00bdf1fded6d569a6576eda4d504
CD <38231409+hotdawg789@users.noreply.github.com>
2022-01-04 22:21:13 -0500

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,161 @@
 // Linking a JavaScript File
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
+
+//
+//section2 vid 12 data types
+//
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'jonas');
+
+javascriptIsFun = 'YES!'
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+
+//
+//section2 vid 13 let,const,var
+//
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+
+
+//
+// section2 vid 14 Basic operators
+//
+
+// math operators
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firtName = 'Jonas';
+const lastName = 'Dummy';
+console.log(firtName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++;
+x--;
+x--;
+console.log(x);
+
+// comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(isFullAge);
+
+
+//
+// section 2 vid 15 operator precedence
+//
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018)
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(averageAge);
+
+
+//
+// section 2 vid 17 string and template literals
+//
+
+const firstName = 'Crease';
+const job = 'teacher';
+const birthYear = 1992;
+const year = 2037;
+
+const crease = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(crease)
+
+const creaseNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(creaseNew)
+
+console.log(`this
+is a
+multi
+line string`)
+
+
+//
+// section 2 vid 18 taking decisions
+//
+
+const age = 15;
+
+if (age >= 18) {
+  console.log('Sarah finna start driving 🚗')
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait ${yearsLeft} years 😊`)
+}
+
+const birthYear = 1991;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century)
+*/
+
+//
+// section 2 vid 20 Type Conversion and Coercion
+//
+
+
+
+
+
+
+
+
+
+
+
 
